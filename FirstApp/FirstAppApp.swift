29ade5db60f7fc83ce8cecfcac14b53aec665db8
Mike Downs <ks7d@verizon.net>
2021-05-28 16:53:22 -0400

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Michael Downs on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
